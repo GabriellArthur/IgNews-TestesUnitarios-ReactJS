0b95120edd0f032327359aafe5d685c49b7dc15f
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-23 20:29:11 -0300

    Testando primeiro componente

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+   testPathIgnorePattern: ["/node_modules","/.next/","/.github/"],
+   setupFilesAfterEnv: [
+      "<rootDir>/src/tests/setupTests.ts"
+   ],
+   transform: {
+      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+   },
+   testEnvironment: 'jsdom'
+}
