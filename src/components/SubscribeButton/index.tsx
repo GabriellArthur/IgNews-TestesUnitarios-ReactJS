b956fc104c7813a069fc122286a9f8f97fb65bd8
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-24 05:53:48 -0300

    attWebHooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -5,40 +5,40 @@ import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 export function SubscribeButton() {
-  const [session] = useSession();
-  const router = useRouter()
+   const [session] = useSession();
+   const router = useRouter()
 
-  async function handleSubscribe() {
-    if (!session) {
-      signIn('github')
-      return;
-    }
+   async function handleSubscribe() {
+      if (!session) {
+         signIn('github')
+         return;
+      }
 
-    if (session.activeSubscription) {
-      router.push('/posts');
-      return;
-    }
+      if (session.activeSubscription) {
+         router.push('/posts');
+         return;
+      }
 
-    try {
-      const response = await api.post('/subscribe')
+      try {
+         const response = await api.post('/subscribe')
 
-      const { sessionId } = response.data;
+         const { sessionId } = response.data;
+         
+         const stripe = await getStripeJs()
 
-      const stripe = await getStripeJs()
+         await stripe.redirectToCheckout({ sessionId })
+      } catch (err) {
+         alert(err.message);
+      }
+   }
 
-      await stripe.redirectToCheckout({ sessionId })
-    } catch (err) {
-      alert(err.message);
-    }
-  }
-
-  return (
-    <button
-      type="button"
-      className={styles.subscribeButton}
-      onClick={handleSubscribe}
-    >
-      Subscribe now
-    </button>
-  );
+   return (
+      <button
+         type="button"
+         className={styles.subscribeButton}
+         onClick={handleSubscribe}
+      >
+         Subscribe now
+      </button>
+   );
 }
