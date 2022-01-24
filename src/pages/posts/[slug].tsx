edfb543cb62f7870eda1ec883f8e336d2f9fde8f
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-24 16:23:50 -0300

    doneee

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req })
   const { slug } = params;
 
-  /*
+  
   if (!session?.activeSubscription) {
     return {
       redirect: {
@@ -50,7 +50,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
       }
     }
   }
-  */
+  
 
   const prismic = getPrismicClient(req)
 
