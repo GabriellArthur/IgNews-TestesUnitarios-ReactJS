624035605cd371d2d12dac0041729949d527f582
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-24 06:35:19 -0300

    stripe?

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,36 +1,38 @@
-import { useSession, signIn } from 'next-auth/client';
+import { signIn, useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
-
 
 export function SubscribeButton() {
   const [session] = useSession();
   const router = useRouter();
 
   async function handleSubscribe() {
-    if (!session) {
+    if(!session){
       signIn('github');
       return;
     }
 
-    if (session.activeSubscription) {
+    if(session.activeSubscription) {
       router.push('/posts');
       return;
     }
 
     try {
-      const response = await api.post('/subscribe')
+      const response = await api.post('/subscribe');
 
       const { sessionId } = response.data;
 
-      const stripe = await getStripeJs()
+      const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout({ sessionId });
-    } catch (err) {
+      await stripe.redirectToCheckout({
+        sessionId
+      });
+    } catch(err) {
       alert(err.message);
     }
+
   }
 
   return (
