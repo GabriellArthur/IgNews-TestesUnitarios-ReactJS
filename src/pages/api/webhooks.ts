a257823e326d3cc92bd761092dcb1f24bac6414b
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-24 14:10:29 -0300

    troca de nome
    export default async
    
    para
    
    export default subscribe;

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -29,7 +29,7 @@ const relevantEvents = new Set([
   'customer.subscription.deleted',
 ])
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     const buf = await buffer(req)
     const secret = req.headers['stripe-signature']
@@ -82,3 +82,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(405).end('Method not allowed')
   }
 }
+
+
+export default webhooks;
