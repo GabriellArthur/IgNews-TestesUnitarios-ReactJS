eb60e7be8bd001cb7add2449f6cae4654c291d52
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-23 21:02:11 -0300

    fix debug

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-   testPathIgnorePattern: ["/node_modules","/.next/","/.github/"],
-   setupFilesAfterEnv: [
-      "<rootDir>/src/tests/setupTests.ts"
-   ],
+   // Ignored files during testing
+   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+ 
+   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
+ 
+   // Transform all files before running tests
    transform: {
-      "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
    },
+   moduleNameMapper: { '\\.(css|less|scss|sass)$': 'identity-obj-proxy' },
+   // indicates which environment the tests are running
    testEnvironment: 'jsdom',
-   moduleNameMapper: {
-      "\\.(scss|css|sass)$": "identity-obj-proxy"
-   }
-}
+ };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,27 @@
+import Image from 'next/image';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
+
 import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src={'/images/logo.svg' as any} alt="ignews" layout="fill" />
         <nav>
-
-          <ActiveLink href="/" activeClassName={styles.active}>
+          <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
           </ActiveLink>
-
-          <ActiveLink href="/posts" activeClassName={styles.active}>
+          <ActiveLink activeClassName={styles.active} href="/posts">
             <a>Posts</a>
           </ActiveLink>
         </nav>
+
         <SignInButton />
       </div>
     </header>
-  )
+  );
 }
